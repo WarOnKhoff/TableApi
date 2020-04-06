050e00efea3c2601a6d99dc0ccf5e7224da762d0
WarOnKhoff <voronkovs.aleksandrs@gmail.com>
2020-04-06 13:53:07 +0300

    Fixed cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 require('dotenv/config')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors())
 app.use(bodyParser.json())
 app.get('/', (req, res) => {
 	res.send(`
